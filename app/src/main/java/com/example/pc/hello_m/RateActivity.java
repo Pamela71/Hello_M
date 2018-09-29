4383a2f92fecb5e80ac2caeb0b9649be919c6258
Pamela71 <374598379@qq.com>
2018-09-29 19:24:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/pc/hello_m/RateActivity.java
@@ -0,0 +1,112 @@
+package com.example.pc.hello_m;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class RateActivity extends AppCompatActivity {
+
+    private final String TAG = "Rate";
+    private float dollarRate = 0.1f;
+    private float euroRate = 0.2f;
+    private float wonRate = 0.3f;
+
+    EditText rmb;
+    TextView show;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate);
+
+        rmb = (EditText) findViewById(R.id.rmb);
+        show = (TextView) findViewById(R.id.show);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("myrate", Activity.MOD);
+
+        dollarRate = sharedPreferences.getFloat("dollar_rate",0.0f);
+        euroRate = sharedPreferences.getFloat("euro_rate",0.0f);
+        wonRate = sharedPreferences.getFloat("won_rate",0.0f);
+        Log.i(TAG, "onCreate: sp dollarRate=" + dollarRate);
+        Log.i(TAG, "onCreate: sp euroRate=" + euroRate);
+        Log.i(TAG, "onCreate: sp wonRate=" + wonRate);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //将新设置的汇率写到SP⾥
+        SharedPreferences sharedPreferences = getSharedPreferences("myrate", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putFloat("dollar_rate",dollarRate);
+        editor.putFloat("euro_rate",euroRate);
+        editor.putFloat("won_rate",wonRate);
+        editor.commit();
+        Log.i(TAG, "onActivityResult: 数据已保存到sharedPreferences");
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void onClick(View btn){
+
+        Log.i(TAG, "onClick: ");
+        String str = rmb.getText().toString();
+        System.out.println(str);
+        Log.i(TAG, "onClick: get str=" + str);
+        float r = 0;
+        if(str.length()>0){
+            r = Float.parseFloat(str);
+        }else{
+            //用户没有输入内容
+            Toast.makeText(this, "请输入内容", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Log.i(TAG, "onClick: r=" + r);
+
+        //计算
+        if(btn.getId()==R.id.dollar){
+            show.setText(String.valueOf(r*dollarRate));
+        }else if(btn.getId()==R.id.euro){
+            show.setText(String.valueOf(r*euroRate));
+        }else{
+            show.setText(String.valueOf(r*wonRate));
+        }
+    }
+
+    public void openOne(View btn){
+        OpenConfig();
+    }
+
+    public void OpenConfig(){
+        Intent config = new Intent(this,ConfigActivity.class);
+        config.putExtra("dollar_rate_key",dollarRate);
+        config.putExtra("euro_rate_key",euroRate);
+        config.putExtra("won_rate_key",wonRate);
+        Log.i(TAG, "openOne: dollarRate=" + dollarRate);
+        Log.i(TAG, "openOne: euroRate=" + euroRate);
+        Log.i(TAG, "openOne: wonRate=" + wonRate);
+        //startActivity(config);
+        startActivityForResult(config,1);
+    }
+
+    public boolean OnCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.rate, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(item.getItemId() == R.id.menu_set){
+            OpenConfig();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
